b381492414439c534db522e7bb52a698be80258c
Sarah Mogin <mogin@work.co>
2022-02-07 18:42:01 -0500

    chore: Update styling of shared component

--- packages/common/components/Text.js
+++ packages/common/components/Text.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Text({ children }) {
-  return <h1>{children}</h1>;
+  return <h1 style={{borderBottom: '1px solid #ccc'}}>{children}</h1>;
 }
 
 export default Text;
