a5f393ffb97ac04d7bfecc8caf4350cb4c9494a0
Sarah Mogin <mogin@work.co>
2022-02-07 19:34:47 -0500

    chore: Breaking change in common used in app1

--- packages/app-2/config-overrides.js
+++ packages/app-2/config-overrides.js
@@ -10,6 +10,7 @@ module.exports = function (config, env) {
         /* transpile (converting to es5) code in src/ and shared component library */
         path.resolve('src'),
         path.resolve('../common'),
+        path.resolve('node_modules/@monorepo-test'),
       ])
     )(config, env)
   );
--- packages/common/components/Text.js
+++ packages/common/components/Text.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Text({ children }) {
-  return <h1 style={{borderBottom: '1px solid #fff'}}>{children}</h1>;
+function Text({ title }) {
+  return <h1 style={{borderBottom: '1px solid #fff'}}>{title}</h1>;
 }
 
 export default Text;
